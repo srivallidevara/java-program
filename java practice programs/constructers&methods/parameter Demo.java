e0a35d3daf447b9d32b3678ad00c2ead34660f2a
srivallidevara <devarasrivalli.25.csm@anits.edu.in>
2026-09-23 16:50:24 +0530

    Create parameter Demo.java

---
+++ java practice programs/constructers&methods/parameter Demo.java
@@ -0,0 +1,16 @@
+```java
+class Calculator {
+
+    void add(int a, int b) {
+        int sum = a + b;
+        System.out.println("Sum: " + sum);
+    }
+
+    public static void main(String[] args) {
+
+        Calculator c = new Calculator();
+
+        c.add(10, 20);
+    }
+}
+```
