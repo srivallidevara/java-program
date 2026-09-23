84a480faa4916e7043912745d55347cbb598f213
srivallidevara <devarasrivalli.25.csm@anits.edu.in>
2026-09-23 21:47:09 +0530

    Create StringEquals ignore case Demo.java

---
+++ Assignments/Assignment 3/StringEquals ignore case Demo.java
@@ -0,0 +1,9 @@
+class Demo {
+    public static void main(String[] args) {
+
+        String a = "Hello";
+        String b = "hello";
+
+        System.out.println(a.equalsIgnoreCase(b));
+    }
+}
