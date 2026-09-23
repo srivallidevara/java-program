e3bcd56b22d0e365c0d87779eb603da2e20599da
srivallidevara <devarasrivalli.25.csm@anits.edu.in>
2026-09-23 16:34:28 +0530

    Update method1.java

--- java practice programs/constructers&methods/method1.java
+++ java practice programs/constructers&methods/method1.java
@@ -23,3 +23,26 @@ class Student {
     }
 }
 ```
+
+
+
+
+```java
+class Car {
+
+    String color;
+
+    // Constructor
+    Car() {
+        color = "Red";
+    }
+
+    public static void main(String[] args) {
+
+        Car c1 = new Car();
+
+        System.out.println("Car color: " + c1.color);
+    }
+}
+```
+
