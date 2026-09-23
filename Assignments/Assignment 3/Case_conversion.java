626c89c40039d864258468236491cf79e07bb612
srivallidevara <devarasrivalli.25.csm@anits.edu.in>
2026-09-23 22:03:05 +0530

    Create Case_conversion.java

---
+++ Assignments/Assignment 3/Case_conversion.java
@@ -0,0 +1,9 @@
+class Demo {
+    public static void main(String[] args) {
+
+        String name = "Hello";
+
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
+    }
+}
