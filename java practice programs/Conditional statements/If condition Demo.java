1a2b39a3f71f042f961ea69de5534c344cadbb2f
srivallidevara <devarasrivalli.25.csm@anits.edu.in>
2026-09-23 18:50:25 +0530

    Create If condition Demo.java

---
+++ java practice programs/Conditional statements/If condition Demo.java
@@ -0,0 +1,29 @@
+public class FoodMenu {
+
+    public static void main(String[] args) {
+
+        int choice = 2;
+
+        switch(choice) {
+
+            case 1:
+                System.out.println("Biryani");
+                break;
+
+            case 2:
+                System.out.println("Fried Rice");
+                break;
+
+            case 3:
+                System.out.println("Noodles");
+                break;
+
+            case 4:
+                System.out.println("Chicken Curry");
+                break;
+
+            default:
+                System.out.println("Invalid choice");
+        }
+   }
+
