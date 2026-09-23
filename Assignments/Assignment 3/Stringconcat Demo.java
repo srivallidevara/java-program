653ca7646d334ea4269bc263bcc704ab5d3585c3
srivallidevara <devarasrivalli.25.csm@anits.edu.in>
2026-09-23 21:39:31 +0530

    Create Stringconcat Demo.java

---
+++ Assignments/Assignment 3/Stringconcat Demo.java
@@ -0,0 +1,11 @@
+class Demo {
+    public static void main(String[] args) {
+
+        String firstName = "Sri";
+        String lastName = "Valli";
+
+        String fullName = firstName.concat(lastName);
+
+        System.out.println(fullName);
+    }
+}
