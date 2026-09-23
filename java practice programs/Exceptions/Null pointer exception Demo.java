8c8ff0e7507606ba6aaaeea01ccba70685225f72
srivallidevara <devarasrivalli.25.csm@anits.edu.in>
2026-09-23 17:54:04 +0530

    Update Null pointer exception Demo.java

--- java practice programs/Exceptions/Null pointer exception Demo.java
+++ java practice programs/Exceptions/Null pointer exception Demo.java
@@ -9,3 +9,25 @@ public class NullDemo {
     }
 }
 ```
+
+
+
+
+```java
+public class NullDemo {
+
+    public static void main(String[] args) {
+
+        String studentName = null;
+
+        try {
+            System.out.println("Student Name: " + studentName);
+            System.out.println("Name Length: " + studentName.length());
+        }
+        catch (NullPointerException e) {
+            System.out.println("Student name is null");
+        }
+    }
+}
+```
+
