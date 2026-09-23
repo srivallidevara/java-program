df4213704fe206273bdea171c129d5db62d82c17
srivallidevara <devarasrivalli.25.csm@anits.edu.in>
2026-09-23 18:33:36 +0530

    Update  Else if Demo.java

--- java practice programs/Conditional statements/ Else if Demo.java
+++ java practice programs/Conditional statements/ Else if Demo.java
@@ -1,1 +1,20 @@
-s
+public class TemperatureDemo {
+
+    public static void main(String[] args) {
+
+        int temperature = 32;
+
+        if(temperature >= 40) {
+            System.out.println("Very Hot");
+        }
+        else if(temperature >= 30) {
+            System.out.println("Hot");
+        }
+        else if(temperature >= 20) {
+            System.out.println("Normal");
+        }
+        else {
+            System.out.println("Cold");
+        }
+    }
+}
