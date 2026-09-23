a28ef9c70415648bfd1f62b35221220ff2351e77
srivallidevara <devarasrivalli.25.csm@anits.edu.in>
2026-09-23 16:28:46 +0530

    Update method1.java

--- java practice programs/constructers&methods/method1.java
+++ java practice programs/constructers&methods/method1.java
@@ -1,1 +1,25 @@
-s
+```java
+class Student {
+
+    String name;
+    int age;
+
+    // Constructor
+    Student() {
+        name = "Srivalli";
+        age = 20;
+    }
+
+    void display() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+
+    public static void main(String[] args) {
+
+        Student s1 = new Student();
+
+        s1.display();
+    }
+}
+```
