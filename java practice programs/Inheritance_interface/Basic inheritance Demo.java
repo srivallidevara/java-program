93937447991e122a9d936c9b2d727e7509279fd5
srivallidevara <devarasrivalli.25.csm@anits.edu.in>
2026-09-23 17:05:47 +0530

    Update Basic inheritance Demo.java

--- java practice programs/Inheritance_interface/Basic inheritance Demo.java
+++ java practice programs/Inheritance_interface/Basic inheritance Demo.java
@@ -1,1 +1,26 @@
-s
+```java
+class Fruit {
+
+    void taste() {
+        System.out.println("Fruit is sweet");
+    }
+}
+
+class Apple extends Fruit {
+
+    void color() {
+        System.out.println("Apple is red");
+    }
+}
+
+public class InheritanceDemo {
+
+    public static void main(String[] args) {
+
+        Apple a = new Apple();
+
+        a.taste();
+        a.color();
+    }
+}
+```
